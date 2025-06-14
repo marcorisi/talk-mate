6ed1131405552cfc00f55df6c6f95ab92b1c68a1
Marco Risi <marco.risi.dev@gmail.com>
2025-06-14 21:54:07 +0200

    Simplify layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,24 @@
-import { Stack } from "expo-router";
+// import { Stack } from "expo-router";
+// 
+// export default function RootLayout() {
+//   return <Stack />;
+// }
 
-export default function RootLayout() {
-  return <Stack />;
+// import { View } from "react-native";
+// import PlayerPlayground from "@/components/PlayerPlayground";
+import TranslationApp from "@/components/v0app";
+
+export default function Index() {
+  return (
+    // <View
+    //   style={{
+    //     flex: 1,
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    //   }}
+    // >
+    //   <PlayerPlayground />
+    // </View>
+    <TranslationApp />
+  );
 }
