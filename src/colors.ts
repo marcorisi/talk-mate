61f4002b1c17a9d20bdeaee40ef70fb5b1c793e5
Marco Risi <marco.risi.dev@gmail.com>
2025-06-14 22:32:39 +0200

    Remove useless comment

--- src/colors.ts
+++ src/colors.ts
@@ -1,5 +1,4 @@
 export const colors = {
-  // Background colors
   background: {
     primary: "#f8f9fa",
     secondary: "#6c757d",
@@ -7,13 +6,11 @@ export const colors = {
     container: "white",
   },
   
-  // Text colors
   text: {
     secondary: "#6c757d",
     placeholder: "#ccc",
   },
   
-  // Accent colors
   accent: {
     primary: "#007bff",
     secondary: "#28a745",
